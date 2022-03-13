c9ecaf188e916c0e1bd096e51f32df113322baff
Mwkimani <modestkymm@gmail.com>
2022-03-13 12:48:55 -0700

    hello world!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+/**
+ * main-entry point
+ * This program prints programming is like building a multilingual puzzle
+ * don't use printf
+ * use the function puts
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+
+}

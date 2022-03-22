bb167e3a1f8fe0bc091ef8a54fa80c5850a83031
Mwkimani <modestkymm@gmail.com>
2022-03-22 10:48:00 -0700

    pointers.arrays

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,13 @@
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
 	int i;
 
-	i=0;
+	i = 0;
 
 	while (s[i] != '\0')
 	{
